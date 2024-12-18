4e2eadb4b3c48ff57295483dc1e0aa777d710094
NikDrosakis <nikosdrosakis@gmail.com>
2024-12-18 10:26:21 +0200

    cli dump updated

--- version.sh
+++ version.sh
@@ -22,21 +22,19 @@ fi
   #`summary` longtext #from git summary
   #`created` datetime NOT NULL DEFAULT current_timestamp()
 #) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci; 
-
 # Check for required parameters
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <commit_message>"
     exit 1
 fi
-
 # Parameters
 COMMIT_MESSAGE=$1
-
 # Navigate to the local repository
 cd "$GSROOT" || exit 1
 
-# Step 1: Get the new version number
-NEW_VERSION=$(mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -D$DB_NAME -se "SELECT MAX(version_tag) + 0.01 FROM versions")
+#1: Get the new version number
+NEW_VERSION=$(mysql -u$DB_USER -p$DB_PASS -D$DB_NAME -se "SELECT MAX(version_tag) + 0.001 FROM versions")
+echo ${NEW_VERSION}
 
 # Check if NEW_VERSION is empty or not a number
 if ! [[ "$NEW_VERSION" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
@@ -44,7 +42,8 @@ if ! [[ "$NEW_VERSION" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
     exit 1
 fi
 
-# Step 2: Get the latest commit summary and total changes
+
+#2: Get the latest commit summary and total changes
 LATEST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 TOTAL_CHANGES=$(git rev-list --count HEAD)
 
@@ -57,9 +56,14 @@ git commit -m "$COMMIT_MESSAGE"
 git push origin main --set-upstream origin main
 #git push origin "$NEW_VERSION"
 
-# Step 3: Insert the new version into the database
+
+#3: Insert the new version into the database
 MYSQL_QUERY="INSERT INTO versions (version_tag, title, summary, total_changes) VALUES ($NEW_VERSION, '$COMMIT_MESSAGE', '$LATEST_COMMIT_MESSAGE', $TOTAL_CHANGES);"
 mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -D$DB_NAME -e "$MYSQL_QUERY"
 
 echo "Git pushed, Versioning completed successfully."
 
+#4 download new version of db to mysqldump with IF NOT EXIST WITHOUT ALTER setup/maria/gen_${NEW_VERSION}.sql
+DUMP_DIR="setup/maria/"
+SQL_FILE="${DUMP_DIR}/gen_${NEW_VERSION}.sql"
+mysqldump -u $DB_USER -p$DB_PASSWORD --no-set-names --skip-triggers --routines --create-options --if-exists $DB_NAME > $SQL_FILE
