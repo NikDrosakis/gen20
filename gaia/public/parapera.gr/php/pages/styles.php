586a367217613d1ea5d5a61cdfe9e90880aa69c0
NikDrosakis <nikosdrosakis@gmail.com>
2025-01-27 11:17:58 +0200

    updated .gitignore

--- gaia/public/parapera.gr/php/pages/styles.php
+++
@@ -1,3 +0,0 @@
-   <button type="button" style="border:none;background:none;" onclick="coo('boxstyle','archieve');location.reload()">Archieve</button>
-   <button type="button" style="border:none;background:none;"  onclick="s.coo('boxstyle','table');location.reload()">Table</button>
-  <div class="flag_books" id="count_<?=$G['page']?>"></div>
